eb517e6b50b960dfe73cc96065101930da12dc6b
colinaluke <19101950@usc.edu.ph>
2023-01-20 16:47:44 +0800

    created routes for create, update, and delete education

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -4,6 +4,7 @@ const auth = require('../../middleware/auth')
 const {Profile} = require('../../models/Profile')
 const {Social} = require('../../models/Social')
 const {Experience} = require('../../models/Experience')
+const {Education} = require('../../models/Education')
 const {User} = require('../../models/User')
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
@@ -154,7 +155,7 @@ router.put('/experience', [auth, [
         const profile = await Profile.findOne({ where: { userId: req.user.id }});
         newExp.profileId = profile.id;
 
-        let experience = await Experience.findOne({ where: { profileId : profile.id} });
+        let experience = await Experience.findOne({ where: { profileId : profile.id, title: title, company: company, from: from} });
 
         if(experience) {
             experience.update(newExp, { where: { profileId:  profile.id, returning: true, upsert: true } });
@@ -164,6 +165,57 @@ router.put('/experience', [auth, [
         }
 
         res.json({profile, experience});
+    } catch(err) {
+        console.error(err.message);
+        res.status(500).send('Server error');
+    }
+});
+
+router.put('/education', [auth, [
+    check('school', 'School is required.').not().isEmpty(),
+    check('degree', 'Degree is required.').not().isEmpty(),
+    check('fieldofstudy', 'Field of study is required.').not().isEmpty(),
+    check('from', 'From date is required.').not().isEmpty(),
+]], async(req, res) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    const {
+        school, 
+        degree, 
+        fieldofstudy, 
+        from, 
+        to, 
+        current,
+        description
+    } = req.body;
+
+    const newEdu = {
+        school: school,
+        degree: degree, 
+        fieldofstudy: fieldofstudy, 
+        from: from, 
+        to: to,
+        current: current,
+        description: description
+    }
+
+    try {
+        const profile = await Profile.findOne({ where: { userId: req.user.id }});
+        newEdu.profileId = profile.id;
+
+        let education = await Education.findOne({ where: { profileId : profile.id, school: school, degree: degree, fieldofstudy: fieldofstudy, from: from} });
+
+        if(education) {
+            education.update(newEdu, { where: { profileId:  profile.id, returning: true, upsert: true } });
+        } else {
+            education = new Education(newEdu);
+            await education.save();
+        }
+
+        res.json({profile, education});
     } catch(err) {
         console.error(err.message);
         res.status(500).send('Server error');
@@ -183,5 +235,35 @@ router.delete('/', auth, async(req, res) => {
         return res.status(500).json({ msg: 'Server error' }); 
     }
 })
+
+router.delete('/experience/:exp_id', auth, async(req, res) => {
+    try {
+        const exp = await Experience.findOne({ where: {id:req.params.exp_id}});
+        if(exp) {
+            await Experience.destroy({ where: { id: req.params.exp_id }});
+        } else {
+            return res.status(400).json({ msg: 'Experience is not found.'});
+        }
+        res.json(exp);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server error');
+    }
+});
+
+router.delete('/education/:edu_id', auth, async(req, res) => {
+    try {
+        const edu = await Education.findOne({ where: {id:req.params.edu_id}});
+        if(edu) {
+            await Education.destroy({ where: { id: req.params.edu_id }});
+        } else {
+            return res.status(400).json({ msg: 'Education is not found.'});
+        }
+        res.json(edu);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server error');
+    }
+});
 // end DELETE profile
 module.exports = router;
